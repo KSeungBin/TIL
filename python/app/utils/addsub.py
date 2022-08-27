0671fbc15e4d8728befd2d24e21628253b9f34fe
{github KSeungBin} <starsbk7@gmail.com>
2022-08-27 09:35:04 +0900

    import user-defined module

---
+++ python/app/utils/addsub.py
@@ -0,0 +1,9 @@
+from module import module_1
+
+a = module_1.add(3,4)
+b = module_1.sub(7,2)
+
+def example(self):
+    pass
+
+
---
+++ python/module/module_1.py
@@ -0,0 +1,10 @@
+from app.utils.addsub import example
+
+def add(a:int, b:int) -> int:
+    return a+b
+
+def sub(a:int, b:int) -> int:
+    return a-b
+
+a = example()
+
