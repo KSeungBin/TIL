de7c61c7f9f99bc22f14d68cf31b5fe53f08fac8
{github KSeungBin} <starsbk7@gmail.com>
2022-09-19 22:43:43 +0900

    add code

--- cv_paper/1. context understanding/SimpleEncoder.py
+++ cv_paper/1. context understanding/SimpleEncoder.py
@@ -35,13 +35,14 @@ class MultiHeadAttention(torch.nn.Module):
      return Z, attention_score
 
 # encoder에는 주로 self-attention이 들어온다
-class Encoder(torch.nn.Module):
-    def __init__(self, hidden_dim, num_head):
+class EncoderLayer(torch.nn.Module):
+    def __init__(self, hidden_dim, num_head, dropout_p=0.5):
        super().__init__()
 
        self.num_head = num_head
        self.hidden_dim = hidden_dim # embedding dimension
        # hidden_dim을 num_head로 나누었을 때 나누어 떨어지지 않으면 오류를 발생시키는 assert문을 넣어주기도 함
+       assert self.att_dim * num_head == self.hidden_dim, "hidden_dim must be divisible by num_head"
 
        self.MHA = MultiHeadAttention()
        
@@ -51,6 +52,17 @@ class Encoder(torch.nn.Module):
        self.W_V = torch.nn.Linear(hidden_dim, hidden_dim, bias=False)
 
        self.W_O = torch.nn.Linear(hidden_dim, hidden_dim, bias=False)
+
+       self.LayerNorm1 = torch.nn.LayerNorm(hidden_dim)
+       self.LayerNorm2 = torch.nn.LayerNorm(hidden_dim)
+
+       self.Dropout = torch.nn.Dropout(p=dropout_p)
+
+       self.Linear1 = torch.nn.Linear(hidden_dim, hidden_dim)
+       self.Linear2 = torch.nn.Linear(hidden_dim, hidden_dim)
+
+       self.Activation = torch.nn.ReLU()
+
 
 
     def to_multihead(self, vector):
@@ -68,9 +80,61 @@ class Encoder(torch.nn.Module):
         K = self.W_K(input_K)
         V = self.W_V(input_V)
 
+        
+        num_batch, num_token_length, hidden_dim = Q.shape
         # split : hidden_dim을 (num_head, att_dim)으로 쪼개기
         Q = self.to_multihead(Q)  # [num_batch, num_head, num_token_length, att_dim]
         K = self.to_multihead(K)
         V = self.to_multihead(V)
 
+        # self-attention 마무리
         Z, attention_score = self.MHA(Q,K,V)
+        Z = Z.permute(0,2,1,3)
+        Z = Z.reshape(num_batch, num_token_length, self.hidden_dim) # 또는 (num_batch, num_token_length, self.num_head*att_dim)
+        Z = self.W_O(Z)
+
+        # encoder를 마무리하려면 residual connection, normalization, feed forward network 필요
+        Z = self.LayerNorm1(self.Activation(Z) + input_Q)
+        Z1 = self.Dropout(Z)
+
+        Z = self.Activation(self.Linear1(Z1)) # feed forward
+        Z = self.Dropout(Z)
+        Z = self.Activation(self.Linear2(Z1))
+        Z = self.Dropout(Z)
+
+        Z = Z + Z1 # residual connection
+
+        Z = self.LayerNorm2(Z) # normalization
+
+        return Z
+
+
+if __name__ == "main":
+    device = torch.device('cpu')
+    num_batch = 16
+    num_head = 2
+    hidden_dim = 64
+    num_token_length = 8
+
+    X = torch.Tensor(torch.randn(num_batch, num_token_length, hidden_dim))
+    print('X.shape: ', X.shape)
+
+    self_attention_encoder = EncoderLayer(hidden_dim=hidden_dim, num_head=num_head)
+
+    Z = self_attention_encoder(input_Q=X, input_K=X, input_V=X)
+    print('Z.shape: ', Z.shape)
+
+
+
+# TRANSFORMER > TransformerEncoder > TransformerEncoderLayer > MultiheadAttention >multi_head_attention_forward > _scaled_dot_product_attention
+# d_model = hidden_dim 
+
+# torch.nn.Transformer(d_model=512, nhead=8, num_encoder_layers=6, num_decoder_layers=6, \
+#  dim_feedforward=2048, dropout=0.1, activation=<function relu>, custom_encoder=None, custom_decoder=None, \
+#  layer_norm_eps=1e-05, batch_first=False, norm_first=False, device=None, dtype=None)
+
+from torch.nn.modules import TransformerEncoderLayer
+
+official_encoder = TransformerEncoderLayer(d_model=hidden_dim, nhead=num_head, dim_feedforward=hidden_dim)
+official_Z = official_encoder(X)
+print('official_Z.shape: ', official_Z.shape)
