2346cb065d825f2fff6c879633bc45a0976def5a
{github KSeungBin} <starsbk7@gmail.com>
2022-09-28 21:29:40 +0900

    optimization using pruning

--- algorithm/python algorithm interview/Non-Linear data structure/8. course_schedule.py
+++ algorithm/python algorithm interview/Non-Linear data structure/8. course_schedule.py
@@ -1,7 +1,7 @@
 import collections
 from typing import *
 # method 1 : detect a cycle using dfs
-def canFinish(self, numCourses:int, prerequisites:List[List[int]]) -> bool:
+def canFinish1(self, numCourses:int, prerequisites:List[List[int]]) -> bool:
     graph = collections.defaultdict(list)
 
     for x, y in prerequisites:
@@ -21,6 +21,42 @@ def canFinish(self, numCourses:int, prerequisites:List[List[int]]) -> bool:
         
         # remove cyclic node after search
         traced.remove(i)
+
+        return True
+    
+    # detect cyclic structure
+    for x in list(graph):
+        if not dfs(x):
+            return False
+    
+    return True
+
+
+# method 2 : optimization using pruning
+def canFinish2(self, numCourses:int, prerequisites:List[List[int]]) -> bool:
+    graph = collections.defaultdict(list)
+    for x, y in prerequisites:
+        graph[x].append(y)
+    
+    traced = set()
+    visited = set()
+
+    def dfs(i):
+        # If it is a cyclic structure, return False
+        if i in traced:
+            return False
+        # Return True if the node has already been visited
+        if i in visited:
+            return True
+        
+        traced.add(i)
+        for y in graph[i]:
+            if not dfs(y):
+                return False
+        # delete circular node after search
+        traced.remove(i)
+        # add visited node after search
+        visited.add(i)
 
         return True
     
