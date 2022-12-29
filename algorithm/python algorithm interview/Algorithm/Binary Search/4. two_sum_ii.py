1d06b528ad91ec764ce88a906a59490055a38576
{github KSeungBin} <starsbk7@gmail.com>
2022-12-29 22:35:34 +0900

    fix: add method2(binary search)

--- algorithm/python algorithm interview/Algorithm/Binary Search/4. two_sum_ii.py
+++ algorithm/python algorithm interview/Algorithm/Binary Search/4. two_sum_ii.py
@@ -2,7 +2,7 @@ from typing import *
 
 
 # method 1: two-pointer
-def twoSum(self, numbers: List[int], target: int) -> List[int]:
+def twoSum1(self, numbers: List[int], target: int) -> List[int]:
     left, right = 0, len(numbers) - 1
     while not left == right:
         if numbers[left] + numbers[right] < target:
@@ -11,3 +11,19 @@ def twoSum(self, numbers: List[int], target: int) -> List[int]:
             right -= 1
         else:
             return left + 1, right + 1
+
+
+# method 2: binary search
+def twoSum2(self, numbers: List[int], target: int) -> List[int]:
+    for k, v in enumerate(numbers):
+        left, right = k + 1, len(numbers) - 1
+        expected = target - v
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if numbers[mid] < expected:
+                left = mid + 1
+            elif numbers[mid] > expected:
+                right = mid - 1
+            else:
+                return k + 1, mid + 1
