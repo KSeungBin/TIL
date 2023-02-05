857ed6951e60d16cd3ea141e97ad361406e84bec
{github KSeungBin} <starsbk7@gmail.com>
2023-02-05 15:18:06 +0900

    fix: add method2(visit once)

--- algorithm/python algorithm interview/Algorithm/Greedy/6. gas_station.py
+++ algorithm/python algorithm interview/Algorithm/Greedy/6. gas_station.py
@@ -1,7 +1,7 @@
 from typing import *
 
 # method 1: visit all
-def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+def canCompleteCircuit1(self, gas: List[int], cost: List[int]) -> int:
     for start in range(len(gas)):
         fuel = 0
         for i in range(start, len(gas) + start):
@@ -16,3 +16,18 @@ def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if can_travel:
             return start
     return -1
+
+
+# method 2: visit once
+def canCompleteCircuit2(self, gas: List[int], cost: List[int]) -> int:
+    if sum(gas) < sum(cost):
+        return -1
+    
+    start, fuel = 0, 0
+    for i in range(len(gas)):
+        if gas[i] + fuel < cost[i]:
+            start = i + 1
+            fuel = 0
+        else:
+            fuel += gas[i] - cost[i]
+    return start
