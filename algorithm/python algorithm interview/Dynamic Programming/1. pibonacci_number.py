11c07fd35af6fb05bdb4386106c85879e815bf9b
{github KSeungBin} <starsbk7@gmail.com>
2023-03-09 19:38:47 +0900

    fix: add method5(matrix)

--- algorithm/python algorithm interview/Dynamic Programming/1. pibonacci_number.py
+++ algorithm/python algorithm interview/Dynamic Programming/1. pibonacci_number.py
@@ -39,3 +39,12 @@ def fib(self, N: int) -> int:
     for i in range(0, N):
         x, y = y, x + y
     return x
+
+
+# method 5: matrix
+import numpy as np
+def fib(n):
+    M = np.matrix([[0, 1], [1, 1]])
+    vec = np.array([[0], [1]])
+
+    return np.matmul(M ** n, vec)[0]
